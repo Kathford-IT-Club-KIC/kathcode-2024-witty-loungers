7fa8b005423a1c1c7210a03cbfedb092ba8681e2
Prakash5209 <prakashkchaudhary5209@gmail.com>
2024-06-28 18:59:42 +0545

    home page done

--- bewell/models.py
+++ bewell/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django_quill.fields import QuillField
 
-# Create your models here.
+class Exercise(models.Model):
+    blog = QuillField()
+    age = models.DecimalField(max_digits=3, decimal_places=0)
+    gender = models.CharField(max_length=255)
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return str(self.age)
--- bewell/views.py
+++ bewell/views.py
@@ -5,3 +5,16 @@ def home(request):
     context = {
     }
     return render(request,'home.html',context)
+
+def survey(request):
+    if request.method == 'POST':
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        height = request.POST.get('height')
+        weight = request.POST.get('weight')
+        print('age',age)
+        print('gender',gender)
+        print('height',height)
+        print('weight',weight)
+    context = {}
+    return render(request,'survey.html',context)
