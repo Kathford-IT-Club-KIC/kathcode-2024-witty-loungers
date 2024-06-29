83c1dcf9ff2104950a6256de065e59674636e9e1
Prakash5209 <prakashkchaudhary5209@gmail.com>
2024-06-29 11:01:16 +0545

    modified

--- bewell/models.py
+++ bewell/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django_quill.fields import QuillField
 
+class Food(models.Model):
+    blog = QuillField()
+    gender = models.CharField(max_length=255)
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return str(self.id)
+
+
 class Exercise(models.Model):
     blog = QuillField()
     age = models.DecimalField(max_digits=3, decimal_places=0)
@@ -10,3 +20,20 @@ class Exercise(models.Model):
 
     def __str__(self):
         return str(self.age)
+    
+
+class Sleep(models.Model):
+    trouble_sleeping = models.BooleanField()
+    title = models.CharField(max_length=255)
+    blog = QuillField()
+
+    def __str__(self):
+        return self.title
+
+
+class Water(models.Model):
+    litre = models.CharField(max_length=10)
+    blog = QuillField()
+
+    def __str__(self):
+        return self.litre
